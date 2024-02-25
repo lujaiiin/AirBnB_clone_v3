c5276674098aa055c8ce2d71deb57806ec0d001e
lujain <lujain@g.com>
2024-02-25 17:43:44 +0000

    lala

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,19 @@ from api.v1.views import app_views
 def status():
     """function"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """get stats fun"""
+    response = {}
+    CY = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+            }
+    for i, k in CY.items():
+        response[k] = storage.count(i)
+    return jsonify(response)
