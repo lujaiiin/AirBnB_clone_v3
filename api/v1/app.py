e8c5d565e01be0a11b7ee603c06a1440a34d4e6b
lujain <lujain@g.com>
2024-02-25 17:58:46 +0000

    lala

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Modules"""
 from flask import Flask
+from flask import jsonify
 import os
 from models import storage
 from api.v1.views import app_views
+
 
 app = Flask(__name__)
 
@@ -12,8 +14,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_db(exception=None):
     """teardown fun"""
-
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """not found function"""
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
