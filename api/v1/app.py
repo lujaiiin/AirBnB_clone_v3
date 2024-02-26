4d9eb9a0774f31fe5ad78fc30cac5818904e8bb4
yasmen <yasmen.khaled2000@gmail.com>
2024-02-26 15:22:03 +0200

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
-""" a script that starts a Flask web application """
-from flask import Flask, blueprints, jsonify
+"""app"""
+
+from os import getenv
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
 from flask_cors import CORS
-import os
-
-HBNB_API_HOST = os.getenv('HBNB_API_HOST')
-HBNB_API_PORT = os.getenv('HBNB_API_PORT')
-
-
-if HBNB_API_HOST is None:
-    HBNB_API_HOST = '0.0.0.0'
-if HBNB_API_PORT is None:
-    HBNB_API_PORT = 5000
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cros = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """teardown"""
+def teardown_app(exception):
+    """request"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """ error 404  """
-    status = {"error": "Not found"}
-    return jsonify(status), 404
+def not_found(error):
+    """ 404 page"""
+    return jsonify({'error': 'Not found'}), 404
 
 
-if __name__ == '__main__':
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+if __name__ == "__main__":
+    """Main"""
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
