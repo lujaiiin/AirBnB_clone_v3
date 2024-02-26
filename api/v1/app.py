7932499ab5bc2d185ca4cf067737a255c039aefc
yasmen <yasmen.khaled2000@gmail.com>
2024-02-26 11:14:56 +0200

    test app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
-"""Modules"""
-from flask import Flask
-from flask import jsonify
-import os
+""" a script that starts a Flask web application """
+from flask import Flask, blueprints, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
+import os
+
+HBNB_API_HOST = os.getenv('HBNB_API_HOST')
+HBNB_API_PORT = os.getenv('HBNB_API_PORT')
+
+
+if HBNB_API_HOST is None:
+    HBNB_API_HOST = '0.0.0.0'
+if HBNB_API_PORT is None:
+    HBNB_API_PORT = 5000
 
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
+cros = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def teardown_db(exception=None):
-    """teardown fun"""
+def teardown_db(exception):
+    """teardown"""
     storage.close()
 
-@app.errorhandler(404)
-def not_found(error):
-    """not found function"""
-    return jsonify({'error': 'Not found'}), 404
 
-if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT',  5000))
-    app.run(host=host, port=port, threaded=True)
+@app.errorhandler(404)
+def page_not_found(error):
+    """ error 404  """
+    status = {"error": "Not found"}
+    return jsonify(status), 404
+
+
+if __name__ == '__main__':
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
