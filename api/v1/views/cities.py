b53ee5b74321c98b88ec997c0448d0781885fe1b
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-02-26 22:44:34 +0300

    cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -50,7 +50,7 @@ def post_city(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    if not request.get_json():
+    if not request.is_json:
         abort(400, description="Not a JSON")
     if 'name' not in request.get_json():
         abort(400, description="Missing name")
@@ -69,7 +69,7 @@ def put_city(city_id):
     if not city:
         abort(404)
 
-    if not request.get_json():
+    if not request.is_json:
         abort(400, description="Not a JSON")
 
     ignore = ['id', 'state_id', 'created_at', 'updated_at']
