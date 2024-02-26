bd46b2416c54b66cadf010f7af77c5438be5516b
lujaiiin <132622956+lujaiiin@users.noreply.github.com>
2024-02-27 00:30:19 +0200

    plas

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" view """
+"""Modules"""
 from flask import jsonify, abort, request, make_response
 from models import storage
 from models.place import Place
@@ -11,7 +11,7 @@ from models.user import User
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
 def get_places(city_id):
-    """ Retrieves """
+    """gety"""
     city = storage.get(City, city_id)
     if not city:
         abort(404)
@@ -23,7 +23,7 @@ def get_places(city_id):
 
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
-    """ Retrieves """
+    """gety"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
@@ -33,7 +33,7 @@ def get_place(place_id):
 @app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_place(place_id):
-    """ Deletes a Place  """
+    """delety """
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
